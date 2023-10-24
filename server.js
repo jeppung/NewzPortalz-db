a5e21bf9bb8cdd2a00a582c21a391aea77eac937
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-24 16:19:27 +0700

    feat: recopying server.js

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-// JSON Server module
+// See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
+
 const server = jsonServer.create();
+
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
 const router = jsonServer.router("db.json");
 
-// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/auth/login?email=:email&password=:password": "/users?email=:email&password=:password",
-  "/auth/user/:id": "/users/:id"
- })
+  jsonServer.rewriter({
+    "/auth/login?username=:username&password=:password":
+      "/users?username=:username&password=:password",
+    "/auth/user/:id": "/users/:id",
+  })
 );
 
-server.use(router);
-// Listen to port
-server.listen(6969, () => {
- console.log("JSON Server is running");
+http: server.use(router);
+server.listen(8000, () => {
+  console.log("JSON Server is running");
 });
 
 // Export the Server API
